03752cf7744630a285a4bd69f7f83369ccd2f867
Sami Ullah <samiullahmalik7474@gmail.com>
2024-12-24 15:09:23 +0500

    2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,7 +10,7 @@ export default async function Home() {
     <div className="text-center text-lg text-green-800 font-bold my-20"><p className="text-red-600 text-2xl">Todos List from the API </p>{limitedTodos.map((brick:any,index:any)=>{
       return(
         
-        <div className="flex justify-between gap-5 w-[50vw] mx-auto"><h1>{index+1}</h1><span className="w-[40vw] text-start"><Link href={`${brick.id}`}>{brick.title}</Link></span></div>
+        <div className="flex justify-between gap-5 w-[50vw] mx-auto" key={brick.id}><h1>{index+1}</h1><span className="w-[40vw] text-start"><Link href={`${brick.id}`}>{brick.title}</Link></span></div>
       )
     })}</div>
   </div>
